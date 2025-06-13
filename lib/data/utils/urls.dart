863f35beaebcf42c829a617674f97059593b038c
siamhossain0 <mdsiamhosaain0@gmail.com>
2025-06-13 17:13:48 +0600

    urls

---
+++ lib/data/utils/urls.dart
@@ -0,0 +1,6 @@
+class Urls{
+
+  static const String _baseUrl = "https://task.teamrabbil.com/api/v1";
+  static const String registration ='$_baseUrl/registration';
+  static const String login ="https://task.teamrabbil.com/api/v1/login";
+}
